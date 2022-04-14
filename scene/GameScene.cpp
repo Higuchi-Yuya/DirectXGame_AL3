2a9d5c90957f183f43020216322aceb1a7a6811f
YuyaHiguchi <k021g1263@g.neec.ac.jp>
2022-04-14 15:26:15 +0900

    保存

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -7,7 +7,7 @@ using namespace DirectX;
 GameScene::GameScene() {}
 
 GameScene::~GameScene() { 
-	delete sprite_;
+	
 	delete model_;
 
 }
@@ -25,12 +25,12 @@ void GameScene::Initialize() {
 	//3Dモデルの生成
 	model_ = Model::Create();
 
-	//// X,Y,Z方向のスケーリングを設定
-	//worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
-	//// X,Y,Z軸周りの回転角を設定
-	//worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};
-	//// X,Y,Z軸周りの平行移動を設定
-	//worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
+	// X,Y,Z方向のスケーリングを設定
+	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
+	// X,Y,Z軸周りの回転角を設定
+	worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};
+	// X,Y,Z軸周りの平行移動を設定
+	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
 
 	//ワールドトランスフォームの初期化
 	worldTransform_.Initialize();
@@ -38,47 +38,25 @@ void GameScene::Initialize() {
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
 
-	//スプライトの生成
-	sprite_ = Sprite::Create(textureHandle_, {100, 50});
-
-	//サウンドデータの読み込み
-	soundDataHandle_ = audio_->LoadWave("se_sad03.wav");
-
-	//音声再生
-	audio_->PlayWave(soundDataHandle_,true);
-
-	
 }
 
-void GameScene::Update() {
-	//スプライトの今の座標を取得
-	XMFLOAT2 position = sprite_->GetPosition();
-	//座標を｛２，０｝移動
-	position.x += 2.0f;
-	position.y += 1.0f;
-	//移動した座標をスプライトに反映
-	sprite_->SetPosition(position);
+void GameScene::Update() { 
 
-	//スペースキーを押した瞬間
-	if (input_->TriggerKey(DIK_SPACE)) {
-	//音声停止
-		audio_->StopWave(voiceHandle_);
-	}
+//デバッグテキストの表示
+	debugText_->SetPos(50, 50);
+	debugText_->Printf(
+  "translation:(%f,%f,%f)", worldTransform_.translation_.x, worldTransform_.translation_.y,
+  worldTransform_.translation_.z);
 
-	////デバッグテキストの表示
-	//debugText_->Print("Kaizokuou ni oreha naru.", 50, 50, 1.0f);
+	debugText_->SetPos(50, 70);
+	debugText_->Printf(
+	  "rotation:(%f,%f,%f)", worldTransform_.rotation_.x, worldTransform_.rotation_.y,
+	  worldTransform_.rotation_.z);
 
-	////書式指定付き表示
-	//debugText_->SetPos(50, 70);
-	//debugText_->Printf("year:%d", 2001);
-
-	//変数の値をインクリメント
-	value_++;
-	//値を含んだ文字列
-	std::string strDebug = std::string("Value:") + std::to_string(value_);
-
-	//デバッグテキストの表示
-	debugText_->Print(strDebug, 50, 50, 1.0f);
+	debugText_->SetPos(50, 90);
+	debugText_->Printf(
+	  "scale:(%f,%f,%f)", worldTransform_.scale_.x, worldTransform_.scale_.y,
+	  worldTransform_.scale_.z);
 }
 
 void GameScene::Draw() {
@@ -122,7 +100,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-	sprite_->Draw();
+	//sprite_->Draw();
 
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
